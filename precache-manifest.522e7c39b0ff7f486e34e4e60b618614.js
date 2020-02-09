7c542006df3998d5175875bc429e452c359cafe3
Iwerzhon <scapegoat.minijeux@gmail.com>
2020-02-09 19:53:41 +0100

    Updates

---
+++ precache-manifest.522e7c39b0ff7f486e34e4e60b618614.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c5e261e5ccba59f9c9c23b3d25a11a",
+    "url": "/gw2-raids/index.html"
+  },
+  {
+    "revision": "24772422ecc7025e5aee",
+    "url": "/gw2-raids/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "29423d743e9d2504dd33",
+    "url": "/gw2-raids/static/js/2.c7887795.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gw2-raids/static/js/2.c7887795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24772422ecc7025e5aee",
+    "url": "/gw2-raids/static/js/main.1c3fdaf6.chunk.js"
+  },
+  {
+    "revision": "805541becfbb9940156e",
+    "url": "/gw2-raids/static/js/runtime-main.2e50269e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gw2-raids/static/media/logo.5d5d9eef.svg"
+  }
+]);
